862eb4a26ed2549ba814bbfc7b3c1fd1fbcb081e
Chris Gramer <gramer3514@gmail.com>
2023-05-14 13:50:34 -0700

    updated schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE employeesData;
+
+USE employeesData;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    
+)
