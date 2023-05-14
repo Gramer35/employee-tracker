92f6fb61bcfe3365d91271718812b8511ed146ce
Chris Gramer <gramer3514@gmail.com>
2023-05-14 14:18:36 -0700

    updated schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,30 @@ CREATE TABLE department (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE role (
-    
+CREATE TABLE roles (
+    id INT NOT NULL AUTO INCREMENT,
+    title VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(10,2),
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    -- necessary?
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO INCREMENT,
+    first_name VARCHAR(50),
+    last_name CARCHAR(50),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id)
+    -- necessary?
+    ON DELETE SET NULL,
+    FOREIGN KEY(manager_id)
+    REFERENCES employee(id)
+    -- necessary?
+    ON DELETE SET NULL
 )
